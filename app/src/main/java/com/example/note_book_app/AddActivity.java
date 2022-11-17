25035589ae8d54bdc9fd7c3e183d0ebab86b4b62
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-11-17 22:14:09 +0200

    Insert Data in Database Table

--- app/src/main/java/com/example/note_book_app/AddActivity.java
+++ app/src/main/java/com/example/note_book_app/AddActivity.java
@@ -3,12 +3,34 @@ package com.example.note_book_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText titleInput, authorInput, pagesInput;
+    private Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        titleInput = findViewById(R.id.title_input);
+        authorInput = findViewById(R.id.author_input);
+        pagesInput = findViewById(R.id.page_input);
+        addButton = findViewById(R.id.add_book);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper DB = new DatabaseHelper(AddActivity.this);
+                  DB.addBook(titleInput.getText().toString().trim(),
+                        authorInput.getText().toString().trim(),
+                        Integer.valueOf(pagesInput.getText().toString().trim()));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/note_book_app/DatabaseHelper.java
+++ app/src/main/java/com/example/note_book_app/DatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.note_book_app;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -30,7 +32,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                COLUMN_TITLE + " TEXT, " +
                COLUMN_AUTHOR + " TEXT, " +
-               COLUMN_PAGES + "INTEGER);";
+               COLUMN_PAGES + " INTEGER);";
 
        db.execSQL(query);
 
@@ -40,5 +42,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public void addBook(String title, String author, int pagesNum){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COLUMN_TITLE, title);
+        contentValues.put(COLUMN_AUTHOR, author);
+        contentValues.put(COLUMN_PAGES, pagesNum);
+
+       long result =  db.insert(TABLE_NAME,null,contentValues);
+
+       if(result == -1) {
+           Toast.makeText(context,"Failed!",Toast.LENGTH_SHORT).show();
+       }else{
+          Toast.makeText(context,"Added Successfully!",Toast.LENGTH_SHORT).show();
+       }
+
     }
 }
